cc955702469078905c55ed89c079442fe3d8cba2
Luca Carrisi <luca.carrisi@bcsoft.net>
2021-08-11 19:28:23 +0200

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useState } from 'react';
+import { BrowserRouter as Router, Link, Redirect, Route, Switch, useParams } from "react-router-dom";
 import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
 
-function App() {
+const App = () => {
+  const name = 'John Doe'
+  const isAuthenticated = true
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to={`/about/${name}`}>About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        {
+        isAuthenticated ? 
+        <>
+        <Route path="/about/:name" component={About} />
+        <Route path="/contact"  component={Contact} />
+        </> : <Redirect to="/" />
+        }
+    </Switch>
+      </main>
+    </Router>
   );
 }
-
 export default App;
---
+++ src/Components/Home.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+
+const animatedComponents = makeAnimated();
+
+interface IselectOption {
+  value: string;
+  label: string;
+}
+const selectItems: IselectOption[] = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
+
+const Home: React.FC = () => {
+  const [selectedOption, setSelectedOption] = useState([] as IselectOption[]);
+
+  return (
+    <>
+      <div style={{ padding: "2rem" }}>
+        <div>Home</div>
+        <Select
+          onChange={(e) => {
+            if (e !== null) {
+              let data: IselectOption[] = e.map((x) => {
+                return { value: x.value, label: x.label };
+              });
+              setSelectedOption(data);
+            }
+          }}
+          isMulti
+          closeMenuOnSelect={true}
+          defaultValue={selectedOption}
+          options={selectItems}
+          components={animatedComponents}
+        />
+        {selectedOption && selectedOption.length > 0 
+            ?   <div>Items selected({selectedOption.length}):
+                    {selectedOption.map((x,i) => 
+                        <div key={i}>
+                            {x.value}
+                        </div>)
+                    }
+                </div>
+            :   <label>No Items selected</label>
+        }
+      </div>
+    </>
+  );
+};
+
+export default Home;
